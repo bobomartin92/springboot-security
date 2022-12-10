da064f22ae5e25e4fa430b689782fc7d15e97451
Bodiseowei Martin Tonweriyai <bmtonweriyai@gmail.com>
2022-12-10 14:13:46 +0100

    first commit

---
+++ src/test/java/dev/decadev/demo/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.decadev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
