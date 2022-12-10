8d6ddaec5fafbe0e058538b60da4e77f74263bf8
Bodiseowei Martin Tonweriyai <bmtonweriyai@gmail.com>
2022-12-10 22:35:50 +0100

    fixes

---
+++ src/main/java/dev/decadev/demo/util/GenPass.java
@@ -0,0 +1,10 @@
+package dev.decadev.demo.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class GenPass {
+
+    public static void main(String[] args) {
+        System.out.println(new BCryptPasswordEncoder().encode("1234"));
+    }
+}
