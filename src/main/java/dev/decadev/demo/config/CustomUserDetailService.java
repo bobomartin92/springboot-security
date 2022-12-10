ed1a5a50b147a446d4117f4ce1d0f75032359d38
Bodiseowei Martin Tonweriyai <bmtonweriyai@gmail.com>
2022-12-10 18:31:53 +0100

    added custom userDetailsService and database

---
+++ src/main/java/dev/decadev/demo/config/CustomUserDetailService.java
@@ -0,0 +1,28 @@
+package dev.decadev.demo.config;
+
+import dev.decadev.demo.model.User;
+import dev.decadev.demo.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@AllArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email: " + email));
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(), user.getPassword(), Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN")));
+    }
+}
---
+++ src/main/java/dev/decadev/demo/model/User.java
@@ -0,0 +1,23 @@
+package dev.decadev.demo.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"email"})
+})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
